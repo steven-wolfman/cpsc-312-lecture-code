db9575c14b30273f6298d1e8fdc8769b1bc87dc8
Steve Wolfman <wolf@mail.ubc.ca>
2021-09-10 10:21:41 -0700

    stack setup for lecture-code

---
+++ Main.hs
@@ -0,0 +1,6 @@
+
+main = do putStrLn "For cabal with sandboxes (e.g., repl.it), run: cabal repl"
+          putStrLn "For stack, run: stack ghci"
+          putStrLn "Just ghci may also work, but you may lack some lectures' packages."
+          putStrLn "Once in the repl, run: :load Lecture1.lhs"
+          putStrLn "Or replace with the lecture you prefer to load!"
